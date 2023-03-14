49fe36589babe4bc52e3f9903e54263c603d7270
Bruna Oliveira <bruna.opdejesus@gmail.com>
2023-03-14 11:44:13 -0300

    14/03

---
+++ aula03.R
@@ -0,0 +1,40 @@
+# exemplo de soma
+2 + 5
+
+# exemplo de sub
+5 - 7
+
+# exemplo de mult
+30 * 2
+
+# exemplo de div
+10 / 2
+
+# exemplo de potência
+3^2
+3**2
+
+# exemplo de raiz
+sqrt(100)
+
+# exemplo de raiz enésima
+8^(1/3)
+
+# exemplo de módulo
+abs(-7)
+abs(7)
+
+# exemplo de resto da divisão
+9 %% 3
+4 %% 3
+
+# exemplo de quociente da divisão
+10 %/% 2
+7 %/% 3
+
+# exemplo de fatorial
+factorial(14)
+
+# exemplo de log
+log(8, base=2)
+log(8, 2)

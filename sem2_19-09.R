346ea82a42086a7c14f3e37c5780fce0a3ec793e
Bruna Oliveira <bruna.opdejesus@gmail.com>
2023-09-19 12:11:35 -0300

    aula 19-09

---
+++ sem2_19-09.R
@@ -0,0 +1,59 @@
+# Exemplo 1
+# Z crítico
+Zc <- (195-200)/(10/sqrt(100))
+Zc
+
+# Z alfa
+Za = qnorm(0.05)
+Za
+
+Zc = (395-400)/(20/sqrt(100))
+Zc
+
+Za = qnorm(0.05/2)
+Za
+
+
+
+
+
+zc = (43500 - 45000) / (5200 / sqrt(30))
+zc
+
+za = qnorm(0.05)
+za
+
+
+
+
+
+
+
+media <- mean(9.1, 9.3, 7.2, 7.5, 13.3, 10.9, 7.2, 9.9, 8.0, 8.6)
+media
+
+zc = (9.1 - 8) / (2 / sqrt(10))
+zc
+
+za = qnorm(0.06/2)
+za
+
+za = qnorm(0.10/2)
+za
+
+
+
+
+
+
+dados = c(180, 172, 175, 160, 170, 171, 160, 172, 171, 182)
+mean(dados)
+
+media <- mean(180, 172, 175, 160, 170, 171, 160, 172, 171, 182)
+media
+
+zc = (180 - 170) / (5 / sqrt(10))
+zc
+
+za = qnorm(0.02, lower.tail=FALSE) 
+za
